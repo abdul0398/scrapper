58c8955be7f3381e913dd621178546fa3b262982
abdul0398 <abdul77789@gmail.com>
2023-12-04 09:43:40 +0530

    Added cron jobs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const start = require('./utils/scrapHandler.js');
 const path = require('path');
+const schedule = require('node-schedule');
 
 
 const port = process.env.PORT || 3000;
@@ -11,4 +12,11 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
 })
+
 start();
+const job = schedule.scheduleJob('0 0 */4 * *', function(){
+  start();
+});
+
+
+
